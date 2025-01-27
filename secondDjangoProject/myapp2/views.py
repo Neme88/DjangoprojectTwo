028e75a79145ce8c19b94cbc6da67d931937d6b4
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-01-27 12:39:53 +0200

    implemented a view function that returns a request

--- secondDjangoProject/myapp2/views.py
+++ secondDjangoProject/myapp2/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 # create your view here
 def home(request):
-    content
+    content = "<html><body><h1>Welcome my people na me yan wetin I yan</h1></body></html>"
+    return HttpResponse(content)
+
 
 
