cc4e330a97bdd3070953f34fa56e2afd8d6d1159
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-01-27 13:05:07 +0200

    I set the url config at the project level

--- secondDjangoProject/myapp2/views.py
+++ secondDjangoProject/myapp2/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 
 # create your view here
 def home(request):
-    content = "<html><body><h1>Welcome my people na me yan wetin I yan</h1></body></html>"
+    content = "<html><body><h1>Welcome my People no one hack my account na me yan wetin I yan:</h1></body></html>"
     return HttpResponse(content)
+
 
 
 
