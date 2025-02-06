4b54c4c37fb2a28b2a416759b871c167005f9aab
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-06 11:27:20 +0200

    added a food manu static files which prints list of available food

--- secondDjangoProject/myapp2/urls.py
+++ secondDjangoProject/myapp2/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home),
-    path('drinks/<str:drink_name>', views.drinks, name='drink_name')
+    path('drinks/<str:drink_name>', views.drinks, name='drink_name'),
+    path('foods/', views.list_Of_Food, name='list_of_food'),
 ]
 
--- secondDjangoProject/myapp2/views.py
+++ secondDjangoProject/myapp2/views.py
@@ -16,6 +16,12 @@ def drinks(request:HttpRequest, drink_name:str) -> HttpResponse:
     choice_of_drink = drink.get(drink_name, "Sorry we do not have that drink")
     return HttpResponse(f"<h2>{drink_name}</h2> {choice_of_drink}" )
 
+def list_Of_Food(request:HttpRequest) -> HttpResponse:
+    food = [ "Paster", "Cereal", "Beans", 'Pork chops', "Plantain"]
+    food_list = "<ul>" + "".join(f"<li>{item}</li>" for item in food)
+    return HttpResponse(f"<h2>List of food</h2>{food_list}")
+
+
 
 
 
