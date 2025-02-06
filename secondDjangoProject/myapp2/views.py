9f7d54628aca80b65d48537c72a7fcd7c1beb468
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-06 13:53:18 +0200

    added a food manu dynamic which show different manu's depending if you choose all the manu  or vegetarian or non-vegetarian manu

--- secondDjangoProject/myapp2/views.py
+++ secondDjangoProject/myapp2/views.py
@@ -16,10 +16,19 @@ def drinks(request:HttpRequest, drink_name:str) -> HttpResponse:
     choice_of_drink = drink.get(drink_name, "Sorry we do not have that drink")
     return HttpResponse(f"<h2>{drink_name}</h2> {choice_of_drink}" )
 
-def list_Of_Food(request:HttpRequest) -> HttpResponse:
-    food = [ "Paster", "Cereal", "Beans", 'Pork chops', "Plantain"]
-    food_list = "<ul>" + "".join(f"<li>{item}</li>" for item in food)
-    return HttpResponse(f"<h2>List of food</h2>{food_list}")
+def list_Of_Food(request:HttpRequest, category:str) -> HttpResponse:
+    food_categories = {
+        'vegetarian': ["cereal", "Bean", "Plaintain"],
+        'non-vegetarian': ["pork chops", "Chicken", "Beef"],
+        'all': ["Paster", "Cereal", "Beans", "Pork chops", "Plaintain", "Chicken", "Beef"]
+}
+# Get the list of foods for the given category, default to 'all' if category is unknown
+    foods = food_categories.get(category, food_categories['all'])
+    # Create a list of foods as HTML
+    food_list = "<ul>" + "".join(f"<li>{item}</li>" for item in foods) + "</ul>"
+    return HttpResponse(f"<h2>Food Category: {category}</h2>{food_list}")
+
+
 
 
 
