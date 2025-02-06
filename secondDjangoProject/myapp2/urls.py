96d8b3215082df24571152f65dbf82aa38d1adeb
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-06 10:20:45 +0200

    added a drinks menu description function for our request response cycle exercise

--- secondDjangoProject/myapp2/urls.py
+++ secondDjangoProject/myapp2/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home),
+    path('drinks/<str:drink_name>', views.drinks, name='drink_name')
 ]
 
--- secondDjangoProject/myapp2/views.py
+++ secondDjangoProject/myapp2/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
+
 
 # create your view here
 def home(request):
     content = "<html><body><h1>Welcome my People no one hack my account na me yan wetin I yan:</h1></body></html>"
     return HttpResponse(content)
+
+def drinks(request:HttpRequest, drink_name:str) -> HttpResponse:
+    drink = {
+        'mocha': 'Is a type of coffee',
+        'tea': 'Is a type of beverage',
+        'lemonade': 'Is a type of refreshment'
+    }
+    choice_of_drink = drink.get(drink_name, "Sorry we do not have that drink")
+    return HttpResponse(f"<h2>{drink_name}</h2> {choice_of_drink}" )
+
+
+
 
 
 
