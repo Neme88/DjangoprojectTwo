b6f3888b4a5e27c4d4fcbed303a12509f9b66f81
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-07 22:21:45 +0200

    add template structure for the app

--- secondDjangoProject/myapp2/urls.py
+++ secondDjangoProject/myapp2/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
+    path('',views.home, name='home'),
     path('drinks/<str:drink_name>', views.drinks, name='drink_name'),
     path('foods/<str:category>/', views.list_Of_Food, name='list_of_food'),
     path('register/', views.register, name='register'),
