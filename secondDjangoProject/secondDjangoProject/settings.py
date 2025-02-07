fdbe759b44b2ff11da372394d843dda0d7faec17
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-07 19:23:58 +0200

    enhanced security by include .env file where all our environmental file will be stored

--- secondDjangoProject/secondDjangoProject/settings.py
+++ secondDjangoProject/secondDjangoProject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,9 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m+i*ulm4o0xh(+nu)f(gu93_z6^#_x_ycfl*o-!wqrgj8zj#vi'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
