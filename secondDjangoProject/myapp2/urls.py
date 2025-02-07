6a03db67ac975a56a8eee8e87479a9ffbed6aa32
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-07 20:58:51 +0200

    implemented routing for the register view class method

--- secondDjangoProject/myapp2/urls.py
+++ secondDjangoProject/myapp2/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.home),
     path('drinks/<str:drink_name>', views.drinks, name='drink_name'),
     path('foods/<str:category>/', views.list_Of_Food, name='list_of_food'),
+    path('register/', views.register, name='register'),
+
 ]
 
