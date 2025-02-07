d21c8b97fa707eba2968f586fa3261eecd1e4798
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-07 20:13:07 +0200

    fixed the be dug that causing the env file to not load properly

--- secondDjangoProject/secondDjangoProject/settings.py
+++ secondDjangoProject/secondDjangoProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 # Initialize environment variables
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(os.path.join(os.path.dirname(__file__), '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
@@ -76,12 +76,10 @@ WSGI_APPLICATION = 'secondDjangoProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+DATABASE_URL = env('DATABASE_URL')
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
 
 
