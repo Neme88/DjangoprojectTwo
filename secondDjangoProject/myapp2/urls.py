1271f3b89afae7f18d2462c9888bfb2d059f4467
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-07 22:39:41 +0200

    included the login view function routing

--- secondDjangoProject/myapp2/urls.py
+++ secondDjangoProject/myapp2/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('drinks/<str:drink_name>', views.drinks, name='drink_name'),
     path('foods/<str:category>/', views.list_Of_Food, name='list_of_food'),
     path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+
 
 ]
 
