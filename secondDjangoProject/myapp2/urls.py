bd212e00cc9e68b754cb92c1e3bfb357be98b42a
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-08 18:08:49 +0200

    implemented the delete, update, and create functionality html

--- secondDjangoProject/myapp2/urls.py
+++ secondDjangoProject/myapp2/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('', views.item_list, name='item_list'),
     path('item/new/', views.item_create, name='item_create'),
     path('item/<int:pk>/edit/', views.item_update, name='item_update'),
+    path('item/<int:pk>/delete', views.item_delete, name='item_delete'),
 ]
 
